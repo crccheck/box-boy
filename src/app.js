3e8d5e3c5477b668ad6fe46edcf4b606fa96ecd6
crccheck <c@crccheck.com>
2017-02-20 20:07:55 +0000

    draw one box

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('hi')
+const d3 = require('d3')
+
+const root = d3.select('#sandbox')
+root.append('div')
+  .style('background', 'blue')
+  .style('width', '500px')
+  .style('height', '500px')
