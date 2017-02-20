63c949023852ec671c69bb5029d06268f00640ee
crccheck <c@crccheck.com>
2017-02-20 21:11:45 +0000

    gimmie color

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const d3 = require('d3')
+window.d3 = d3  // DEBUG
 
-const MAX_ELEMENTS = 20
+const MAX_ELEMENTS = 40
 const root = d3.select('#sandbox')
 
 const choices = [
@@ -10,13 +11,14 @@ const choices = [
   [260, 460],  // 16:9
 ]
 
+let colorGlobal = 0
+
 function add() {
   // Trim extra elements
   const existing = root.selectAll('div')
   const count = existing.data().length  // TODO what's the real way to do this?
-  console.log(count)
   if (count > MAX_ELEMENTS) {
-    console.log('delteme', existing.filter((d, i) => i < count - MAX_ELEMENTS).remove())
+    existing.filter((d, i) => i < count - MAX_ELEMENTS).remove()
   }
 
   const vw = window.innerWidth
@@ -24,11 +26,13 @@ function add() {
 
   root.append('div')
     .datum(choices[0 | Math.random() * choices.length])
-    .style('background', 'rgba(0, 0, 255, 0.2)')
+    .style('background', d3.cubehelix(colorGlobal += 10, 0.9, 0.7, 0.3).toString())
     .style('width', ([w, h]) => `${w}px`)
     .style('height', ([w, h]) => `${h}px`)
     .style('left', ([w, h]) => `${0 | Math.random() * (vw - w)}px`)
     .style('top', ([w, h]) => `${0 | Math.random() * (vh - h)}px`)
+
+  colorGlobal = colorGlobal % 360
 }
 
 add()
