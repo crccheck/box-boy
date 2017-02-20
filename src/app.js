3ff04ab6c98003ba1faec822c81db2552e6ba4fa
crccheck <c@crccheck.com>
2017-02-20 20:15:03 +0000

    add many boxes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const d3 = require('d3')
 
 const root = d3.select('#sandbox')
 
-root.append('div')
-  .datum([500, 500])
-  .style('background', 'rgba(0, 0, 255, 0.2)')
-  .style('width', ([w, h]) => `${w}px`)
-  .style('height', ([w, h]) => `${h}px`)
+function add() {
+  root.append('div')
+    .datum([500, 500])
+    .style('background', 'rgba(0, 0, 255, 0.2)')
+    .style('width', ([w, h]) => `${w}px`)
+    .style('height', ([w, h]) => `${h}px`)
+}
+
+add()
+setInterval(add, 2000)
